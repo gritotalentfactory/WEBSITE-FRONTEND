ea2341bbda08bc08b093bbe2738ef23699dfca07
mutiat <mutiat900@gmail.com>
2024-01-20 16:09:52 +0100

    created the navbar

--- app/page.js
+++ app/page.js
@@ -1,8 +1,31 @@
 import React from "react";
-import NavBar from "@/components/layout/navbar";
+import NavBar from "/components/layout/navbar";
+import Logo from '/assets/logo.png';
+import Link from "next/link";
+import Image from "next/image";
+
 
 const page = () => {
-  return <div>page</div>;
+  return <div>
+    <nav className="w-full bg-black fixed text-[#CBB26A]">
+    <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+    <Link href="./">
+    <Image src={Logo} alt="Some text" className="lg:inline-flex items-center p-2 mr-4" />
+              </Link>
+              <ul className="md:flex items-center justify-between pt-4 md:pt-0">
+                <li><a className="md:p-4 py-3 px-0 block text-md hover:text-white" href="#">Hire Talents</a></li>
+                <li><a className="md:p-4 py-3 px-0 block text-md hover:text-white" href="#">GRITO Academy</a></li>
+                <li><a className="md:p-4 py-3 px-0 block text-md hover:text-white" href="#">Contact Us</a></li>
+               
+            </ul>
+              
+           
+              </div>
+          
+              </nav>
+
+  </div>;
+  
 };
 
 export default page;
