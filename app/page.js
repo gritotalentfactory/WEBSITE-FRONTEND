be0eeb13d558d0508ccdaced832b6e3f05c2edf3
mutiat <mutiat900@gmail.com>
2024-01-20 22:27:57 +0100

    created the footer section

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import React from "react";
 import NavBar from "/components/layout/navbar";
 import Logo from '/assets/logo.png';
+import Youtube from '/assets/youtube.png'
+import Linkedin from '/assets/linkedin.png'
 import Link from "next/link";
 import Image from "next/image";
 
@@ -24,7 +26,10 @@ const page = () => {
           
               </nav>
 
+
+            
   </div>;
+  
   
 };
 
--- components/layout/footer/index.js
+++ components/layout/footer/index.js
@@ -1,8 +1,72 @@
-import React from 'react'
-
+import React from 'react';
+import Logo from '/assets/logo.png';
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+        <footer className=" px-8 divide-y bg-black   text-white dark:text-gray-100">
+	<div className="container flex flex-col p-10  mx-auto space-y-8 lg:flex-row lg:space-y-0">
+
+  <div className="mb-6 md:mb-0 p-6 max-w-1/3">
+  <Link href="./">
+    <Image src={Logo} alt="Some text" className="lg:inline-flex items-center p-2 mr-4" />
+              </Link>
+              <div className="mt-4">
+    <p className="text-xs">Carrer Sicilia 190 1ero 2°, </p>
+<p className="text-xs">08013, Ciudad de Barcelona,</p>
+<p className="text-xs">Espana.</p>
+</div>
+<div className="mt-4">
+    <p className="text-xs"> Africa: +234 802 919 7097</p>
+<p className="text-xs">Email: emeka@grito.africa</p>
+</div>
+<div className="mt-4">
+<p className="text-xs">Europe: +346 310 385 88</p>
+<p className="text-xs">Email: gustavo@grito.africa</p>
+</div>
+        </div>
+        <div class=" left-1/2 ml-6 w-0.5 h-70 bg-white"></div>
+      <div className="p-6 ">
+       <h3 className="text-[#CBB26A] p-6 text-bold">OFFICE HOUR</h3>
+              <div className="mt-4">
+    <p className="text-xs">9am - 4pm (Mon - Sat, WAT) </p>
+ <button type="button"  className="text-[#CBB26A] hover:text-white  rounded-full px-8 py-2 border border-[#CBB26A] mt-14">Chat With Us</button>
+</div>
+   
+ 
+    </div>
+    <div class=" left-1/2 ml-6 w-0.5 h-70 bg-white"></div>
+    <div className="p-6">
+      
+              <div className="mt-4 pt-14">
+    <p className="text-xs w-64 leading-7">At GRITO Talent Factory, our motto says,
+‘Never Waste Talent’. We take African Tech
+talents one step closer to their dream tech 
+jobs with top European Companies.</p>
+
+<div className="flex ml-32 mt-16 ">
+  <Link href="./">
+    <Image src={Youtube} alt="Youtube icon" className=" items-center p-2 w-8 " />
+              </Link>
+              
+              <Link href="./">
+    <Image src={Linkedin} alt="Linkedin icon" className="items-center p-2 w-8 " />
+              </Link></div>
+
+    
+        
+        
+ 
+    </div>
+    
+    </div>
+    
+    </div>
+    <p className="py-6 text-sm text-center dark:text-gray-400">© ALL RIGHTS RESERVED 2023 | GRITO TALENT FACTORY.</p>
+
+		
+
+    </footer>
+    </div>
   )
 }
 
