83a2e402f683c17b089515b87b33500a0278a317
Nky237 <ezikenkemdilijane@gmail.com>
2024-01-22 12:37:47 +0100

    added the view more

--- app/pages/(dashboard)/dashboard/page.jsx
+++ app/pages/(dashboard)/dashboard/page.jsx
@@ -4,6 +4,8 @@ import React from "react";
 import Marquee from "react-fast-marquee";
 import { getCurrentDate } from "@/utils/dateFormat";
 import Card from "@/components/card";
+import { Table } from "@/components/table";
+import Button from "@/components/ui/button";
 
 const page = () => {
   const userData = JSON.parse(Cookies.get("userData"));
@@ -18,11 +20,37 @@ const page = () => {
         </p>
       </Marquee>
 
-      <div className="flex justify-between">
+      <div className="flex justify-between mt-12">
         <Card Text={"Total Talent"} Number={"20"} />
         <Card Text={"Deleted Talent"} Number={"20"} />
         <Card Text={"Active Talent"} Number={"20"} />
         <Card Text={"Inactive Talent"} Number={"20"} />
+      </div>
+      <div className="flex item-end justify-end gap-4 my-16">
+        <Button
+          size="md"
+          variant="primary"
+          text={"Download CSV"}
+          disabled={false}
+          fullWidth={false}
+        />
+        <Button
+          size="md"
+          variant="primary"
+          text={"Download PDF"}
+          disabled={false}
+          fullWidth={false}
+        />
+        <Button
+          size="md"
+          variant="primary"
+          text={"+ Create Talent"}
+          disabled={false}
+          fullWidth={false}
+        />
+      </div>
+      <div>
+        <Table />
       </div>
     </div>
   );
