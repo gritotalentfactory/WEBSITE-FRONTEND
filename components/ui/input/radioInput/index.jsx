2334c13afb4748cc5eecccfa477b1c64184ccc57
Buzz-brain <chinomsochristian03@gmail.com>
2024-06-12 06:02:53 +0100

    Updated Frontend

--- components/ui/input/radioInput/index.jsx
+++ components/ui/input/radioInput/index.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 const RadioInput = ({ value, name, text, onChange, checked }) => {
   return (
-    <div className="bg-white py-2 px-5 h-[40px] flex items-center gap-3 rounded-md mx-3 text-black">
+    <div className="flex items-center gap-3 mx-3 my-2">
       <input 
         type="radio" 
         value={value} 
         name={name} 
         onChange={onChange} 
         checked={checked} 
+        id={`${name}-${value}`} // Add an id to associate with the label
+        className="form-radio h-4 w-4 text-blue-600 transition duration-150 ease-in-out" // Tailwind classes for styling
       />
-      <label htmlFor={name} className="text-black">
+      <label htmlFor={`${name}-${value}`} className="text-black">
         {text}
       </label>
     </div>
